9a9dc9516744810c4bb8a06adfbfc09715cdcb27
mehmetcinarpro <mehmetcinarpro@gmail.com>
2021-08-27 07:35:40 +0200

    add rounds controller

---
+++ ExactScore/Controllers/RoundsController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ExactScore.Data;
+using ExactScore.Data.Entities;
+
+namespace ExactScore.Controllers
+{
+    public class RoundsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RoundsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Rounds
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Rounds.Include(r => r.Season).OrderBy(r => r.Closed).ThenBy(r => r.OrderNumber);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Rounds/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var round = await _context.Rounds.FindAsync(id);
+            if (round == null)
+            {
+                return NotFound();
+            }
+            ViewData["SeasonId"] = new SelectList(_context.Seasons, "Id", "Name", round.SeasonId);
+            return View(round);
+        }
+
+        // POST: Rounds/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,OrderNumber,Closed,SeasonId")] Round round)
+        {
+            if (id != round.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(round);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoundExists(round.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SeasonId"] = new SelectList(_context.Seasons, "Id", "Name", round.SeasonId);
+            return View(round);
+        }
+
+        private bool RoundExists(int id)
+        {
+            return _context.Rounds.Any(e => e.Id == id);
+        }
+    }
+}
